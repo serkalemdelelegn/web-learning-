79d22116ba162ab47b34d3464862d1ed4127488d
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2024-08-06 10:34:06 +0300

    idk

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let driverLicence=false;
+const passTest=true;
+
+if (passTest) driverLicence=true;
+if (driverLicence) console.log("i can drive:D");
---
+++ js/script.js
@@ -0,0 +1,8 @@
+//'use strict';
+
+let hasDriverLIcence = false;
+const passTest = true;
+ if (passTest) hasDriverLIcence=true;
+ if (hasDriverLIcence) console.log('I can driver');
+
+ 
